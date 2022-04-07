b6478de776fe2a98d7771c31c8410ab177a8d678
francisco952 <4franciscoplazas@gmail.com>
2022-04-07 10:44:32 -0500

    Agregando formularios de actualizar y capturadores js

--- public/js/pop-up-forms.js
+++ public/js/pop-up-forms.js
@@ -2,6 +2,10 @@ const button = document.querySelector('#Openform');
 const poup = document.querySelector('.content-form-subject');
 const closed = document.querySelector('.popup-close-window');
 
+const buutonact = document.querySelector('#OpenAct');
+const poupact = document.querySelector('.content-form-subject-actul');
+const closedact = document.querySelector('.popup-close-window-act');
+
 closed.addEventListener('click', () => {
     poup.style.display = 'none';
 });
@@ -15,7 +19,22 @@ poup.addEventListener('click', e => {
     }
 });
 
+closedact.addEventListener('click', () => {
+    poupact.style.display = 'none';
+});
+buutonact.addEventListener('click', () => {
+    poupact.style.display = 'block';
+});
+poupact.addEventListener('click', e => {
+    // console.log(e);
+    if (e.target.className === 'content-form-subject-actul') {
+        poupact.style.display = 'none';
+    }
+});
+
+
 let archive = document.querySelector('#img');
     archive.addEventListener('change',()=>{
         document.querySelector('#originalfilename').innerText = archive.files[0].name;
     });
+    /* Tener en cuenta que generara un error de no reconocer la funcion addEventListener de la imagen porque no la estamos llamando en el ejs*/
