3a08fa6d7a5d31bdf3d7b58793ba10da3690d555
DavidLugo847 <krt847@gmail.com>
2022-04-12 23:32:16 -0500

    Agregando Modals al modulo

--- public/js/ventas.js
+++ public/js/ventas.js
@@ -1,8 +1,106 @@
-var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
-    keyboard: false
+$(document).ready(function() {
+    $('#tableDetalle', '#tableFacturar', '#tableCart').DataTable({
+        language: {
+            "decimal": ",",
+            "thousands": ".",
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sSearch": "Buscar:",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "sProcessing": "Cargando..."
+        }
+    });
+    $('#tableFacturar').DataTable({
+        bFilter: false,
+        language: {
+            "decimal": ",",
+            "thousands": ".",
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sSearch": "Buscar:",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "sProcessing": "Cargando..."
+        }
+    });
+    $('#tableCart').DataTable({
+        bFilter: false,
+        language: {
+            "decimal": ",",
+            "thousands": ".",
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sSearch": "Buscar:",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "sProcessing": "Cargando..."
+        }
+    });
+    $('#tableAddProd').DataTable({
+        bFilter: false,
+        language: {
+            "decimal": ",",
+            "thousands": ".",
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sSearch": "Buscar:",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "sProcessing": "Cargando..."
+        }
+    });
 });
 
-function AbrirFrmUsuario() {
-    myModal;
-    myModal.toggle();
+var facturar = new bootstrap.Modal(document.getElementById('modalFacturar'), { keyboard: false });
+
+function compranueva() {
+    facturar;
+    facturar.toggle();
+}
+var detalle = new bootstrap.Modal(document.getElementById('modalDetalle'), { keyboard: false });
+
+function mostrarDetalle() {
+    detalle;
+    detalle.toggle();
+}
+var newVenta = new bootstrap.Modal(document.getElementById('modalNewVenta'), { keyboard: false });
+
+function nuevaVenta() {
+    newVenta;
+    newVenta.toggle();
+}
+var addProd = new bootstrap.Modal(document.getElementById('modalAddProd'), { keyboard: false });
+
+function addProdVen() {
+    addProd;
+    addProd.toggle();
 }
