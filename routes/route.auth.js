d79794490c1706c0c71de24a4959164008f35dc5
AlejandroC26 <cubillosa862mail.com>
2022-04-17 16:30:06 -0500

    editar-password

--- routes/route.auth.js
+++ routes/route.auth.js
@@ -4,5 +4,6 @@ let controllerAuth = require('../controllers/controller.auth')
 
 routeAuth.post('/login', controllerAuth.logIn);
 routeAuth.post('/logout', controllerAuth.logOut);
+routeAuth.post('/change-password', controllerAuth.changePassword)
 
 module.exports = routeAuth;
