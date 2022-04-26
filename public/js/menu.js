4312d72fd5ebd25acc4c26aae6f7693a6a8c1d30
AlejandroC26 <cubillosa862mail.com>
2022-04-25 21:46:15 -0500

    menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,8 +1,17 @@
 const btnMenu = document.querySelector('#btn-menu');
 const menu = document.querySelector('#menu');
-btnMenu.addEventListener('click', function () {
-	menu.classList.toggle('mostrar');
-});
-$('.bt-close').click(function () {
-	menu.classList.toggle('mostrar');
-});
+function animNav(){
+	let navbar = document.getElementById('main-navbar');
+	let wrapper = document.getElementById('wrapper-menu');
+	if(navbar.className) {
+		wrapper.style.display = 'none';
+		return navbar.removeAttribute('class');
+	}
+	wrapper.style.display = 'block';
+	return navbar.className += "show-navbar";
+}
+$('.submenu').click(function(){$(this).children('.children').slideToggle();});
+$('.bt-close').click(function () {animNav();});
+btnMenu.addEventListener('click', function () {animNav();});
+document.querySelector('.open-close').addEventListener('click', ()=>{animNav();});
+document.getElementById('wrapper-menu').addEventListener('click', ()=>{animNav();});
